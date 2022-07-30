74b09bbac0ef3537b5297858ca874db4e7d22376
Mihail Dimoski <mihaildimoski@gmail.com>
2022-07-30 22:01:22 +0200

    Caps Lock functuality online - green color class added on Caps Lock button

--- core.js
+++ core.js
@@ -4,7 +4,7 @@ console.log(letters);
 
 // Na klik, ako e true da prodolzhi so golemi bukvi, ako e false so mali
 
-const checkbox = document.querySelector("#caps").checked = false;
+let checkbox = false;
 console.log(checkbox);
 const icon = document.querySelector("i");
 
@@ -30,14 +30,21 @@ letters.forEach((e) => {
       const row = document.createElement("br");
       paragraph.appendChild(row);
       written.push(row);
-    } else if (e === "Caps Lock") {
-      checkbox === false ? (checkbox = true) : (checkbox = false);
+    } else if (e.innerText === "Caps Lock") {
+      if ((checkbox = !checkbox)) {
+        checkbox = true;
+        e.classList.add('checkbox-green');
+      } else {
+        checkbox = false;
+        e.classList.remove('checkbox-green');
+
+      }
       console.log(checkbox);
     } else {
       // TRY ONE
       checkbox
-        ? (paragraph.innerHTML += e.innerHTML.toLowerCase)
-        : (paragraph.innerHTML += e.innerHTML);
+        ? (paragraph.innerHTML += e.innerHTML)
+        : (paragraph.innerHTML += e.innerHTML.toLowerCase());
 
       // TRY TWO
 
