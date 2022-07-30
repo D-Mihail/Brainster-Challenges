fbadec30aeac72225a67584ee93d4780cbcaac61
Mihail Dimoski <mihaildimoski@gmail.com>
2022-07-30 23:21:12 +0200

    Cleared the code a lil bit

--- core.js
+++ core.js
@@ -1,27 +1,17 @@
 const letters = document.querySelectorAll("button");
 const paragraph = document.querySelector("p");
-console.log(letters);
-
 // Na klik, ako e true da prodolzhi so golemi bukvi, ako e false so mali
-
 let checkbox = false;
-console.log(checkbox);
-const icon = document.querySelector("i");
-
 letters.forEach((e) => {
   e.addEventListener("click", () => {
     let written = Array.from(paragraph.innerText);
-    if (e.innerHTML === "backspace") {
-      // SAKAM DA GO PRETVORAM TEKSTOT VO NIZA, DA GO IZBRISHAM POSLEDNIOT, DA GO SKLOPAM VO EDEN ELEMENT I DA GO VRATAM VO PARAGRAFOT
-      console.log(written);
+    if (e.innerText === "backspace") {
+      // Sakam da go pretvoram tekstot vo niza, da go izbrisham posledniot chlen, da go sklopam vo eden element i da go vratam kako vrednost vo paragrafot
       written.pop();
-      console.log(written);
       let joinedSentence = written.join("");
       while (written.length) {
         written.pop();
       }
-      console.log(joinedSentence);
-
       paragraph.innerHTML = joinedSentence;
     } else if (e.innerText === "SPACE") {
       let space = " ";
@@ -33,30 +23,15 @@ letters.forEach((e) => {
     } else if (e.innerText === "Caps Lock") {
       if ((checkbox = !checkbox)) {
         checkbox = true;
-        e.classList.add('checkbox-green');
+        e.classList.add("checkbox-green");
       } else {
         checkbox = false;
-        e.classList.remove('checkbox-green');
-
+        e.classList.remove("checkbox-green");
       }
-      console.log(checkbox);
     } else {
-      // TRY ONE
       checkbox
-        ? (paragraph.innerHTML += e.innerHTML)
-        : (paragraph.innerHTML += e.innerHTML.toLowerCase());
-
-      // TRY TWO
-
-      //     if (checkbox = false) {
-      //         icon.classList.add("checkbox-round")
-      //   paragraph.innerHTML += e.innerHTML;
-
-      //     }
-      //      else {
-      //   paragraph.innerHTML += e.innerHTML.toUpperCase;
-      //   console.log(e.innerHTML);
-      // }
+        ? (paragraph.innerHTML += e.innerText)
+        : (paragraph.innerHTML += e.innerText.toLowerCase());
     }
   });
 });
